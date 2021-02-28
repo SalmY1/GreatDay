88d943b32d5077aa6fbf3000058399226c07784f
SalmY1 <79806541+SalmY1@users.noreply.github.com>
2021-02-28 19:19:42 +0000

    Add files via upload

---
+++ YS2_Notebook.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## <center>
+#     <img src="https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/Logos/organization_logo/organization_logo.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# In[1]:
+
+
+1+1
+
+
+# My Title
+
+# How are you doing
+
+# In[7]:
+
+
+50/6
+
+
+# In[8]:
+
+
+80*3
+
+
+# In[5]:
+
+
+200*50
+
+
+# In[4]:
+
+
+70+2
+
+
+# In[ ]:
+
+
+
+
